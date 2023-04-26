3bb14599107996b2c403d785b6cd56df755f3f1f
SabaG63 <sgach2005@gmail.com>
2023-04-26 23:47:42 +0400

    Add landing page

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const sr = ScrollReveal();
+
+sr.reveal('.header', {
+    origin: 'top',
+    distance: '30px',
+    duration: 1100,
+    delay: 500
+});
+sr.reveal('.container-left', {
+    origin: 'left',
+    distance: '30px',
+    duration: 1100,
+    delay: 500
+});
+sr.reveal('.container-right', {
+    origin: 'right',
+    distance: '30px',
+    duration: 1100,
+    delay: 500
+});
